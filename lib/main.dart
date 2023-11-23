192886110344a05eee755db4ce14c8a4e7cfe6b5
KhalidBG78dem <khalidjj78200@icloud.com>
2023-11-23 16:16:43 +0100

    premiere page fonctionnel

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,42 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(Container(
-    decoration: BoxDecoration(
+    decoration: const BoxDecoration(
       color: Color.fromRGBO(225, 141, 255, 100),
     ),
-    child: Center(
-      child: Text(
-        'Fit-match',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          color: Colors.black,
-          fontFamily: 'bukhari',
-          // Assurez-vous que la police est correctement chargée dans votre projet
-        ),
+    child: const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Fit',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'bukhari',
+              fontSize: 63,
+            ),
+          ),
+          Text(
+            '-',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'grandcru',
+              fontSize: 113,
+            ),
+          ),
+          Text(
+            'Match',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'bukhari',
+              fontSize: 63,
+            ),
+          ),
+        ],
       ),
     ),
   ));
