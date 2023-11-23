792434f1cdae2eaf9b05346df312cf27f6d44425
KhalidBG78dem <khalidjj78200@icloud.com>
2023-11-23 13:33:33 +0100

    pull

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class
 void main() {
   runApp(Container(
-    color: Colors.,
+    decoration: BoxDecoration(
+      color: Color.fromRGBO(225, 141, 255, 100),
+    ),
     child: Center(
       child: Text(
         'Fit-match',
         textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.black,
+          fontFamily: 'bukhari',
+          // Assurez-vous que la police est correctement chargée dans votre projet
+        ),
       ),
     ),
   ));
